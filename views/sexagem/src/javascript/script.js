186826721432c22f4b15f05fbf0885e7d3b8e1a5
Micael <trabalhodigitaltek@gmail.com>
2024-11-05 10:39:27 -0300

    1.0

---
+++ views/sexagem/src/javascript/script.js
@@ -0,0 +1,30 @@
+// Efeitos de Fade
+function handleAnimations() {
+  const elements = document.querySelectorAll('.fadeL, .fadeR');
+
+  ScrollReveal().clean(); // Limpa animações anteriores
+
+  elements.forEach(element => {
+    const revealConfig = {
+      duration: 2000,
+      reset: true,
+      origin: window.innerWidth <= 1200 ? 'bottom' : (element.classList.contains('fadeL') ? 'left' : 'right'),
+      distance: '20%'
+    };
+
+    // Utilizando requestAnimationFrame para garantir a atualização
+    requestAnimationFrame(() => {
+      ScrollReveal().reveal(element, revealConfig);
+    });
+  });
+}
+
+// Chamar a função quando a página carregar
+window.onload = handleAnimations;
+
+// Garante que a página role para o topo após todos os recursos serem carregados
+$(window).on("load", function () {
+  setTimeout(function () {
+    window.scrollTo(0, 0);
+  }, 0);
+});
